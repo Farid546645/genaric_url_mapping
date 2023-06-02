abe7923add0f83720fe1ae01e8aa5406f9f19616
Farid546645 <karumanchipeera546645@gmail.com>
2023-06-02 13:07:34 +0530

    first commit

---
+++ app15/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def genaric(request):
+    return render(request,'genaric.html')
+
+def manasa(request):
+    return render(request,'manasa.html')
